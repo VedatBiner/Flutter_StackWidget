35e8e24f2050552029483e47f84ad9c6a546ee7a
Vedat Biner <vbiner@gmail.com>
2022-04-23 16:58:04 +0300

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,249 @@
+// Tasarım Örneği - Biraz daha Widget ağacı
+// Veri akışı - Inherited Widget
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+
+  @override
+  void initState() {
+    super.initState();
+    ogrenciler.add("init");
+  }
+
+  var sinif = 5;
+  var baslik = "Öğrenciler";
+  var ogrenciler = ["Mehmet", "Vedat", "Zeynep"];
+
+  void yeniOgrenciEkle(String yeniOgrenci){
+    setState(() {
+      ogrenciler = [...ogrenciler, yeniOgrenci];
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    print("MyHomePageState Build ...");
+    return Scaffold(
+      resizeToAvoidBottomInset: false,
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: SinifBilgisi(
+        sinif: sinif,
+        baslik: baslik,
+        ogrenciler: ogrenciler,
+        yeniOgrenciEkle: yeniOgrenciEkle,
+        child: Stack(
+          fit: StackFit.expand,
+          children: const [
+            ArkaPlan(),
+            Positioned(
+              top: 100,
+              left: 10,
+              right: 10,
+              child: Sinif()
+            ),
+            Positioned(
+              bottom: 20,
+              left: 10,
+              right: 10,
+              child: OgrenciEkleme()
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class SinifBilgisi extends InheritedWidget {
+  const SinifBilgisi({
+    Key? key,
+    required Widget child,
+    required this.sinif,
+    required this.baslik,
+    required this.ogrenciler,
+    required this.yeniOgrenciEkle,
+  }) : super(key: key, child: child);
+
+  final int sinif;
+  final String baslik;
+  final List<String> ogrenciler;
+  final void Function(String yeniOgrenci) yeniOgrenciEkle;
+
+  static SinifBilgisi of(BuildContext context) {
+    final SinifBilgisi? result = context.dependOnInheritedWidgetOfExactType<SinifBilgisi>();
+    assert(result != null, 'No SinifBilgisi found in context');
+    return result!;
+  }
+
+  @override
+  bool updateShouldNotify(SinifBilgisi old) {
+    return sinif != old.sinif ||
+        baslik != old.baslik ||
+        ogrenciler != old.ogrenciler ||
+        yeniOgrenciEkle !=old.yeniOgrenciEkle;
+  }
+}
+
+class Sinif extends StatelessWidget {
+  const Sinif({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifBilgisi = SinifBilgisi.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      mainAxisAlignment: MainAxisAlignment.center,
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: <Widget>[
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Icon(
+              Icons.star,
+              color: Colors.red,
+            ),
+            Text(
+              "${sinifBilgisi.sinif}. Sınıf",
+              textScaleFactor: 2,
+            ),
+            const Icon(
+              Icons.star,
+              color: Colors.red,
+            ),
+          ],
+        ),
+        Text(
+          sinifBilgisi.baslik,
+          textScaleFactor: 1.5,
+        ),
+        OgrenciListesi(),
+
+      ],
+    );
+  }
+}
+
+class OgrenciListesi extends StatelessWidget {
+  const OgrenciListesi({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifBilgisi = SinifBilgisi.of(context); //sınıf bilgisi referansı
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children:[
+        for (final o in sinifBilgisi.ogrenciler)
+          Text(
+              o
+          ),
+      ],
+    );
+  }
+}
+
+class OgrenciEkleme extends StatefulWidget {
+
+  const OgrenciEkleme({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  State<OgrenciEkleme> createState() => _OgrenciEklemeState();
+}
+
+class _OgrenciEklemeState extends State<OgrenciEkleme> {
+  final controller = TextEditingController();
+
+  @override
+  void dispose(){
+    controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final sinifBilgisi = SinifBilgisi.of(context);
+    return Column(
+      mainAxisSize: MainAxisSize.min,
+      children: [
+        TextField(
+          controller: controller,
+          onChanged: (value) {
+            setState(() {
+            });
+          },
+        ),
+        Align(
+          alignment: Alignment.bottomLeft,
+          child: ElevatedButton(
+            onPressed: controller.text.isEmpty? null :  (){
+              final yeniOgrenci = controller.text;
+              sinifBilgisi.yeniOgrenciEkle(yeniOgrenci);
+              controller.text = "";
+            },
+            child: const Text(
+              "Ekle",
+            ),
+          ),
+        ),
+      ],
+    );
+  }
+}
+
+class ArkaPlan extends StatelessWidget {
+  const ArkaPlan({
+    Key? key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.blue,
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 8.0, vertical: 16.0),
+        child: Container(
+          width: 50,
+          height: 100,
+          color: Colors.red,
+        ),
+      ),
+    );
+  }
+}
